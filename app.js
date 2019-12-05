528e691a885aa70f6a9ec13754a2ea16730efcf0
Лапшова Анастасия Геннадьевна <lapshova_ag@FKR.ru>
2019-12-05 10:49:50 +0300

    Мастер теперь получает значения от воркеров

--- app.js
+++ app.js
@@ -4,23 +4,56 @@ var client = redis.createClient(16323, 'redis-16323.c17.us-east-1-4.ec2.cloud.re
 client.auth('U4PFZ7III5GogbXHENNwSjVFvliCLVrz', function(err, doc){
     if(err)
         throw err;
-    // else if(doc === "OK"){
-    //     console.log("Authenticated");
-    // }
 });
 
 client.on('connect', function () {
-    console.log('Connected to Redis...');
+    console.log('Redis client connected...');
 });
-let total = 0;
+
+function Integrate(array, dx, f) {
+    var a = array[0];
+    var b = array[1];
+    n = (b - a) / dx;
+    Area = 0;
+    for (i = 1; i <= n; i++) {
+        x0 = a + (i - 1) * dx;
+        x1 = a + i * dx;
+        Ai = dx * (f(x0) + f(x1)) / 2.;
+        Area = Area + Ai
+    }
+    return Area;
+}
+
+function f(x) {
+    return -0.25 * Math.pow(x, 2) + x + 4;
+}
+
+var cluster = require('cluster');
+var http = require('http');
+const numCPUs = require('os').cpus().length;
+var dx = 1;
+var Integral_Value = 0;
+const ab = [1, 5];
+const start = ab[0];
+const end = ab[1];
+const interval = (end - start) / numCPUs;
+
+if(cluster.isMaster)
+{	
+	for (var i = 0; i < numCPUs; i++)
+	{
+	    cluster.fork();
+	}
+}
+else {waitForPush();}
 
 function waitForPush() {
-    client.brpop(["resData", 0], function (err, reply) {
-        var result = parseFloat(reply[1]);
-        console.log(result);
-        total += result;
-        console.log("Total value is  " + total);
+    client.brpop(["mydata", 0], function (err, reply) {
+        res_redis = reply[1].split(",");
+        var redis_array = [parseInt(res_redis[0]), parseInt(res_redis[1])];
+        Integral_Value = Integrate(redis_array, dx, f);
+        client.lpush(["resData", Integral_Value]);
+        console.log('Value: ', Integral_Value);
         waitForPush();
     })
 }
-waitForPush();
--- kvapp.js
+++ kvapp.js
@@ -4,37 +4,12 @@ var client = redis.createClient(16323, 'redis-16323.c17.us-east-1-4.ec2.cloud.re
 client.auth('U4PFZ7III5GogbXHENNwSjVFvliCLVrz', function(err, doc){
     if(err)
         throw err;
-    // else if(doc === "OK"){
-    //     console.log("Authenticated");
-    // }
 });
 
-function Integrate(array, dx, f) {
-    var a = array[0];
-    var b = array[1];
-    n = (b - a) / dx;
-    Area = 0;
-    for (i = 1; i <= n; i++) {
-        x0 = a + (i - 1) * dx;
-        x1 = a + i * dx;
-        Ai = dx * (f(x0) + f(x1)) / 2.;
-        Area = Area + Ai
-    }
-    return Area;
-}
-
-function f(x) {
-    return -0.25 * Math.pow(x, 2) + x + 4;
-}
-
+let total = 0;
 var cluster = require('cluster');
 var http = require('http');
 const numCPUs = require('os').cpus().length;
-const pid = process.pid;
-var mydata = [];
-var dx = 1;
-var Total = 0;
-var Integral_Value = 0;
 const ab = [1, 5];
 const start = ab[0];
 const end = ab[1];
@@ -45,25 +20,29 @@ if (cluster.isMaster) {
     masterProcess();
 } else {
     childProcess();
+    
 }
 function masterProcess() {
     console.log(`Master ${process.pid} is running`);
     for (var i = 0; i < numCPUs; i++) {
         console.log(`Forking process  ${i}...`);
-        cluster.fork();
+        
         client.lpush(["mydata", out[i].toString()]);
-    }
+    }cluster.fork();
 }
 
 function childProcess() {
-    function waitForPush() {
-        client.brpop(["mydata", 0], function (err, reply) {
-            res_redis = reply[1].split(",");
-            var redis_array = [parseInt(res_redis[0]), parseInt(res_redis[1])];
-            Integral_Value = Integrate(redis_array, dx, f);
-            client.lpush(["resData", Integral_Value]);
-            waitForPush();
-        })
-    }
-    waitForPush();
+    console.log('Answers:');
+    waitForRead();
+}
+
+function waitForRead()
+{
+    client.brpop(["resData", 0], function (err, reply) {
+        var result = parseFloat(reply[1]);
+        console.log(result);
+        total += result;
+        console.log("Total value is  " + total);
+        waitForRead();
+    })
 }
